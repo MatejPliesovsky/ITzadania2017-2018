0f458b81dcea879e95d53dbfd5c086fc80d175d5
MatejPliesovsky <matej.pliesovsky@akademiasovy.sk>
2017-11-25 11:52:14 +0100

    Updated function for age calculating from date and reverse back to date

--- script.js
+++ script.js
@@ -34,12 +34,14 @@ function myCreateTable() {
   var meno = row.insertCell(0);
   var priezvisko = row.insertCell(1);
   var dob = row.insertCell(2);
-  var pohlavie = row.insertCell(3);
-  var row5 = row.insertCell(4);
+  var ee = row.insertCell(3);
+  var pohlavie = row.insertCell(4);
+  var row5 = row.insertCell(5);
 
   meno.innerHTML = $("#meno").val();
   priezvisko.innerHTML = $("#priezvisko").val();
-  dob.innerHTML = $("#date").val() + '<span id="exact_age">Age</span>';
+  dob.innerHTML = "<div class=\"birthday\">" + $("#date").val() + "</div>";
+  ee.innerHTML = dateToAge();
   pohlavie.innerHTML = document.querySelector('input[name = "pohlavie"]:checked').value;
   row5.innerHTML = '<button onclick="myDeleteTable(this)" class="btn btn-danger">X</button>';
 
@@ -50,38 +52,41 @@ function myDeleteTable() {
   document.getElementById("listTable2").deleteRow(this);
 };
 
-function switcher() {
-  var str = document.getElementById("calculate").innerHTML;
-  var res = str.replace("date", "exact_age");
-  document.getElementById("calculate").innerHTML = res;
-}
-
 $(document).ready(function() {
   $("#calculate").click(function() {
-    var mdate = $("#date").val().toString();
-    var yearThen = parseInt(mdate.substring(0, 4), 10);
-    var monthThen = parseInt(mdate.substring(5, 7), 10);
-    var dayThen = parseInt(mdate.substring(8, 10), 10);
-
-    var today = new Date();
-    var birthday = new Date(yearThen, monthThen - 1, dayThen);
-
-    var differenceInMilisecond = today.valueOf() - birthday.valueOf();
-
-    var year_age = Math.floor(differenceInMilisecond / 31536000000);
-    var day_age = Math.floor((differenceInMilisecond % 31536000000) / 86400000);
-
-    var month_age = Math.floor(day_age / 30);
-
-    day_age = day_age % 30;
-
-    if (isNaN(year_age) || isNaN(month_age) || isNaN(day_age)) {
-      $("#exact_age").text("Error!");
-    } else {
-      $("#exact_age").html("<div id=\"age\">" + year_age + "</div> ");
-    }
+    if(calculate.checked){
+    $(".birthday").css({ 'display': "none" });
+    $(".exage").css({ 'display': "block" })
+  }else {
+    $(".birthday").css({ 'display': "block" });
+    $(".exage").css({ 'display': "none" })}
+  })
   });
-});
+
+function dateToAge() {
+  var mdate = $("#date").val().toString();
+  var yearThen = parseInt(mdate.substring(0, 4), 10);
+  var monthThen = parseInt(mdate.substring(5, 7), 10);
+  var dayThen = parseInt(mdate.substring(8, 10), 10);
+
+  var today = new Date();
+  var birthday = new Date(yearThen, monthThen - 1, dayThen);
+
+  var differenceInMilisecond = today.valueOf() - birthday.valueOf();
+
+  var year_age = Math.floor(differenceInMilisecond / 31536000000);
+  var day_age = Math.floor((differenceInMilisecond % 31536000000) / 86400000);
+
+  var month_age = Math.floor(day_age / 30);
+
+  day_age = day_age % 30;
+
+  if (isNaN(year_age) || isNaN(month_age) || isNaN(day_age)) {
+    return "Error!";
+  } else {
+    return "<div class=\"exage\" style=\"display: none\">" + year_age + "</div> ";
+  }
+};
 
 function storeData() {
   if (localStorage) {
